37ac0fafbd6605aadec41ab33c99815203781f86
Alex Johnson <alex.johnson293@gmail.com>
2022-10-30 10:42:05 -0700

    create addContact method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+// Business logic for AddressBook
+
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+}
+
+// Business logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -7,3 +18,5 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
