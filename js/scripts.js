e61191273f400037531c4be1a0ca26df35c4e68c
Alex Johnson <alex.johnson293@gmail.com>
2022-10-31 14:46:03 -0700

    updated through monday lessons

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,30 @@ Contact.prototype.fullName = function() {
 // this global variable is OK as it's supposed to mimic a database
 let addressBook = new AddressBook();
 
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
+function displayContactDetails(event) {
+  const contact = addressBook.findContact(event.target.id);
+  document.querySelector(".first-name").innerText = contact.firstName;
+  document.querySelector(".last-name").innerText = contact.lastName;
+  document.querySelector(".phone-number").innerText = contact.phoneNumber;
+  document.querySelector("div#contact-details").removeAttribute("class");
+  document.querySelector("button.delete").setAttribute("id", contact.id);
+  document.querySelector("div#contact-details").removeAttribute("class");
+}
+
 function handleFormSubmission(event) {
   event.preventDefault();
   //gathers user input & stores in variable
@@ -55,9 +79,23 @@ function handleFormSubmission(event) {
   //appends to addressbook
   addressBook.addContact(newContact);
   // prints
-  console.log(addressBook.contacts);
+  listContacts(addressBook);
+  //this resets the input field values to null
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
 }
+
+function handleDelete(event) {
+  addressBook.deleteContact(event.target.id);
+  document.querySelector("button.delete").removeAttribute("id");
+  document.querySelector("div#contact-details").setAttribute("class", "hidden");
+  listContacts(addressBook);
+}
+
 
 window.addEventListener("load", function (){
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("div#contacts").addEventListener("click", displayContactDetails);   
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
 });
