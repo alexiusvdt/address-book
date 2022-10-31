c10dd7cf0f5b7e01ac31b8b347d47994985ab915
Alex Johnson <alex.johnson293@gmail.com>
2022-10-31 13:27:52 -0700

    update for creation of UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
-// Business logic for AddressBook
-
-//why do we do curly braces here?
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
 
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
-}
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
 
 AddressBook.prototype.findContact = function(id) {
   if (this.contacts[id] !== undefined) {
-      return this.contacts[id];
+    return this.contacts[id];
   }
   return false;
-}
+};
 
 AddressBook.prototype.deleteContact = function(id) {
   if (this.contacts[id] === undefined) {
@@ -29,9 +27,9 @@ AddressBook.prototype.deleteContact = function(id) {
   }
   delete this.contacts[id];
   return true;
-}
+};
 
-// UI logic for Contacts
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -40,6 +38,26 @@ function Contact(firstName, lastName, phoneNumber) {
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+};
+
+// User Interface Logic ---------
+// this global variable is OK as it's supposed to mimic a database
+let addressBook = new AddressBook();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  //gathers user input & stores in variable
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  //creates newContact object using above
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  //appends to addressbook
+  addressBook.addContact(newContact);
+  // prints
+  console.log(addressBook.contacts);
 }
 
-
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
